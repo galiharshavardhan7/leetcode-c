1d4d11fd732fcf51c219114fde77bf59dc301edd
galiharshavardhan7 <galiharshavardhan759@gmail.com>
2026-08-26 01:46:57 +0530

    278_FirstBadVersion

---
+++ 05-Binary-Search/278_FirstBadVersion.c
@@ -0,0 +1,19 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int left = 1;
+    int right = n;
+    
+    while (left < right) {
+        int mid = left + (right - left) / 2; // Prevents 32-bit integer overflow
+        
+        if (isBadVersion(mid)) {
+            right = mid; // First bad version is at mid or earlier
+        } else {
+            left = mid + 1; // Bad version must be after mid
+        }
+    }
+    
+    return left;
+}
