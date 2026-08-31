6154320ddc06b085dcde97c3533245e36545b911
galiharshavardhan7 <galiharshavardhan759@gmail.com>
2026-08-31 21:48:40 +0530

    Add 100. Same Tree solution in C

--- 06-Trees/100_same_tree.c
+++ 06-Trees/100_same_tree.c
@@ -1,3 +1,6 @@
+#include <stdbool.h>
+#include <stddef.h>
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -6,24 +9,16 @@
  *     struct TreeNode *right;
  * };
  */
-#include <stdbool.h>
 
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    // Both nodes are NULL -> trees match at this branch
     if (p == NULL && q == NULL) {
         return true;
     }
-    
-    // One node is NULL and the other is not -> structural mismatch
     if (p == NULL || q == NULL) {
         return false;
     }
-    
-    // Values do not match
     if (p->val != q->val) {
         return false;
     }
-    
-    // Recursively check left and right subtrees
     return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 }
